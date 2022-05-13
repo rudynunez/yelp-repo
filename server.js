ffe9226010da37447bda1138b552b5585e262bd4
rudynunez <rudy.nunez@gmail.com>
2022-05-12 23:16:21 -0400

    411 - 412
    Lessons 411 -412
    
    created seeds/cities.js
                  index.js
                  seedHelpers.js
    
    created database seeding logic using sample data modules
    
    logic consists of first deleting the database then seeding a random
    selection of entries concatenated from the sample data modules to the
    database

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+// mongoose connection
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+  console.log('Mongoose CONNECTED');
+};
+
+const app = express();
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -9,6 +19,11 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: "My Backyard", description: "cheap camping!" });
+    await camp.save();
+    console.log(camp);
+});
 
 
 
