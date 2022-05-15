65504613f3b0d051ace7f1d9ac497ffc6f4fba16
rudynunez <rudy.nunez@gmail.com>
2022-05-15 19:29:34 -0400

    416 continued
    Completed post route for editing campgrounds
    
    changed package.json from:
    
    "scripts": {
        "test": "echo \"Error: no test specified\" && exit 1",
        "start": "node server.js"
      }
    
      to:
    
      "scripts": {
        "test": "echo \"Error: no test specified\" && exit 1",
        "start": "nodemon server.js"
      }

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 // mongoose connection
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/yelp-camp');
-  console.log('Mongoose CONNECTED');
+    await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+    console.log('Mongoose CONNECTED');
 };
 
 const app = express();
@@ -16,7 +16,10 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -24,7 +27,9 @@ app.get('/', (req, res) => {
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
+    res.render('campgrounds/index', {
+        campgrounds
+    });
 });
 
 // SHOW
@@ -34,9 +39,9 @@ app.get('/campgrounds/new', async (req, res) => {
 });
 
 app.post('/campgrounds', async (req, res) => {
-   const campground = new Campground(req.body.campground);
-   await campground.save();
-res.redirect(`/campgrounds/${campground._id}`);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${ campground._id }`);
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -48,6 +53,15 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
 });
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground })
+    res.redirect(`/campgrounds/${ campground._id }`);
+});
+
+
+
 
 
 
