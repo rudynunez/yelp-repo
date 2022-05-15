a009a0c1bad1a3ab2679d2ff3d325d4f127db06b
rudynunez <rudy.nunez@gmail.com>
2022-05-15 18:57:50 -0400

    416. Added edit route and view

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -43,7 +44,10 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campground });
 });
 
-
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+});
 
 
 
