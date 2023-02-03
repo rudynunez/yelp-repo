caf9b3fc0cd2533948a7b7a221c26d02a3752518
rudynunez <rudy.nunez@gmail.com>
2023-02-03 11:27:25 -0500

    494. Flash success partial

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -49,6 +49,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
+    req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${ campground._id }`);
 }));
 
@@ -56,6 +57,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndDelete(id);
+    req.flash('danger', 'Successfully deleted campground!');
     res.redirect('/campgrounds');
 }));
 
--- server.js
+++ server.js
@@ -46,6 +46,7 @@ app.use(flash());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
+    res.locals.danger = req.flash('danger');
     res.locals.error = req.flash('error');
     next();
 });
