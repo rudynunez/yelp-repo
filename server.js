7173de7fbed3a94967989539049f0340e9adf000
rudynunez <rudy.nunez@gmail.com>
2023-02-02 23:16:19 -0500

    Moved review routes

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const {campgroundSchema, reviewSchema} = require('./validationSchemas');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
 
 const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 // mongoose connection
 main().catch(err => console.log(err));
@@ -19,7 +16,6 @@ async function main() {
 };
 
 const app = express();
-
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -31,39 +27,13 @@ app.use(express.urlencoded({
 
 app.use(methodOverride('_method'));
 
- const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
- };
 
 app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
 
 app.get('/', (req, res) => {
     res.render('home');
 });
-
-
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
-   const campground = await Campground.findById(req.params.id);
-   const review = new Review(req.body.review);
-   campground.reviews.push(review);
-   await review.save();
-   await campground.save();
-   res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);    
-}));
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
@@ -76,5 +46,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(3007, () => {
-    console.log("Serving on Port 3007")
+    console.log("Serving on Port 3007");
 });
