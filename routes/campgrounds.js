540c6a63ee0d75eabc1aad4f5918820bff236e3a
rudynunez <rudy.nunez@gmail.com>
2023-02-07 13:16:03 -0500

    520. Added author to campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -32,12 +32,14 @@ router.get('/new', isLoggedIn, (req, res) => {
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     req.flash('success', 'Successfully made a new campground!');
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     res.redirect(`/campgrounds/${ campground._id }`);
 }));
 
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    console.log(campground)
     if(!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
