fda6b1133fb4c9fc7e007d9e4ad1f38e22c07103
rudynunez <rudy.nunez@gmail.com>
2023-02-10 15:14:00 -0500

    528. a fancy way to restructure routes

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,13 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const users = require('../controllers/users');
 
-// const router = require('./campgrounds');
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/register', users.renderRegister);
-
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), users.login);
 
 router.get('/logout', users.logout); 
 
