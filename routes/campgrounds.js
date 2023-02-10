5474dd747f773273595d779126b57442d3eefe00
rudynunez <rudy.nunez@gmail.com>
2023-02-10 18:30:21 -0500

    523. Multer Middlware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,10 +5,16 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("IT WORKED!");
+    });
 
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
