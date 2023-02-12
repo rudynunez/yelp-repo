c58e814a1a161eb94657912059bed2c80b8de0d1
rudynunez <rudy.nunez@gmail.com>
2023-02-11 21:44:50 -0500

    Fixed and simplified index.ejs image

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,8 @@ const seedDB = async () => {
            author: '63e28bc9ce97b37bd333d350',
            location: `${cities[random1000].city}, ${cities[random1000].state}`,
            title: `${sample(descriptors)} ${sample(places)}`,
-           images: [{url: 'https://source.unsplash.com/collection/483251', filename: 'image' + random1000}],
+           images: [{url: 'https://source.unsplash.com/collection/483251', filename: 'image' + random1000}, 
+                    {url: 'https://source.unsplash.com/collection/483251', filename: 'image' + random1000}],
            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea similique quam aperiam maiores asperiores nihil fuga qui officiis delectus! Accusantium enim natus corporis. Architecto veniam minus reprehenderit dolorum aliquam corrupti.',
            price
         });
