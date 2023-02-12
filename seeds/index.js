1825288fa992be5acff1b19d7a5a5f54969b9bd5
rudynunez <rudy.nunez@gmail.com>
2023-02-11 20:20:44 -0500

    experimented with changing seeds file and image displays

--- seeds/index.js
+++ seeds/index.js
@@ -14,14 +14,14 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++){
+    for(let i = 0; i < 5; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
            author: '63e28bc9ce97b37bd333d350',
            location: `${cities[random1000].city}, ${cities[random1000].state}`,
            title: `${sample(descriptors)} ${sample(places)}`,
-           image: 'https://source.unsplash.com/collection/483251',
+           images: [{url: 'https://source.unsplash.com/collection/483251', filename: 'image' + random1000}],
            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea similique quam aperiam maiores asperiores nihil fuga qui officiis delectus! Accusantium enim natus corporis. Architecto veniam minus reprehenderit dolorum aliquam corrupti.',
            price
         });
