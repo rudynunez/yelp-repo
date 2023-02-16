991866caf8b54aa6aa3f5cfc406952c0a6caa934
rudynunez <rudy.nunez@gmail.com>
2023-02-16 15:13:10 -0500

    540. Adding upload to edit page

--- seeds/index.js
+++ seeds/index.js
@@ -21,8 +21,18 @@ const seedDB = async () => {
            author: '63e28bc9ce97b37bd333d350',
            location: `${cities[random1000].city}, ${cities[random1000].state}`,
            title: `${sample(descriptors)} ${sample(places)}`,
-           images: [{url: 'https://source.unsplash.com/collection/483251', filename: 'image' + random1000}, 
-                    {url: 'https://source.unsplash.com/collection/483251', filename: 'image' + random1000}],
+           images: [{
+            url: 'https://res.cloudinary.com/dqvnoupcd/image/upload/v1676165281/workdir/mx503m1thh8uzukkfbhl.png',
+            filename: 'workdir/mx503m1thh8uzukkfbhl',
+          },
+          {
+            url: 'https://res.cloudinary.com/dqvnoupcd/image/upload/v1676165281/workdir/kenlc5l6bumnxmwo8eir.png',
+            filename: 'workdir/kenlc5l6bumnxmwo8eir',
+          },
+          {
+            url: 'https://res.cloudinary.com/dqvnoupcd/image/upload/v1676165282/workdir/vaieziwbccrf9j0fhs9l.png',
+            filename: 'workdir/vaieziwbccrf9j0fhs9l',
+          }],
            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea similique quam aperiam maiores asperiores nihil fuga qui officiis delectus! Accusantium enim natus corporis. Architecto veniam minus reprehenderit dolorum aliquam corrupti.',
            price
         });
