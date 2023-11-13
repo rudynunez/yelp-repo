1f8209577ea181a4af05c32f7a2063dc8990e4c2
rudynunez <rudy.nunez@gmail.com>
2023-11-13 12:48:42 -0500

    569. Adding custom popups to cluster map

--- models/campground.js
+++ models/campground.js
@@ -2,6 +2,17 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+// const ImageSchema = new Schema({
+//     url: String,
+//     filename: String
+// });
+
+// ImageSchema.virtual('thumbnail').get(function () {
+//     return this.url.replace('/upload', '/upload/w_200');
+// });
+
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema ({
     title: String,
     images: [
@@ -34,6 +45,11 @@ const CampgroundSchema = new Schema ({
             ref: 'Review'
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+            <p>${this.description.substring(0, 60)}...</p>`
 });
 
 CampgroundSchema.post('findOneAndDelete', async function(doc) {
