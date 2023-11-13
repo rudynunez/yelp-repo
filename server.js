64b8c6185ac7e919d4d4f1f9b57f61e199062574
rudynunez <rudy.nunez@gmail.com>
2023-11-13 17:22:54 -0500

    583. Added helmet security

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+
 const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
@@ -60,6 +62,7 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
