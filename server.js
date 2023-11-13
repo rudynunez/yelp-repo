99b819ce48676e63c3c2a23a036b24ef1ea0ec72
rudynunez <rudy.nunez@gmail.com>
2023-11-13 17:03:39 -0500

    579. Sanitizing HTML with Joi

--- server.js
+++ server.js
@@ -66,7 +66,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.warning = req.flash('warning');
--- validationSchemas.js
+++ validationSchemas.js
@@ -1,25 +1,35 @@
-const { number } = require('joi');
-const Joi = require('joi');
+// const { number } = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
 
-// const campgroundSchema = Joi.object({
-//     campground: Joi.object({
-//         title: Joi.string().required(),
-//         location: Joi.string().required(),
-//         image: Joi.string().required(),
-//         price: Joi.number().required().min(0),
-//         description: Joi.string().required()
-//     }).required()
-// });
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
 
-// module.exports = campgroundSchema;
+const Joi = BaseJoi.extend(extension)
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
-        location: Joi.string().required(),
-        // image: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 });
@@ -27,6 +37,6 @@ module.exports.campgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
+        body: Joi.string().required().escapeHTML()
     }).required()
-});
+})
